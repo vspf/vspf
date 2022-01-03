fde2ccfd8b2c9d06b3523c4a70bfa6d38c0236aa
Davide Franco <482177+dfranco@users.noreply.github.com>
2022-01-03 08:19:10 +0100

    Add Request class file

---
+++ core/http/request.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Vspf\Core\Http;
+
+class Request
+{
+    private $parameters = [];
+    
+    /**
+     * __construct
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->parameters['GET'] = $_GET;
+        $this->parameters['POST'] = $_POST;
+    }
+            
+    /**
+     * getParameter
+     *
+     * @param  string $method
+     * @param  string $parameter
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function getParameter( $method, $parameter, $default = null)
+    {
+        if (isset($this->parameters[$method][$parameter])) {
+            return htmlspecialchars($this->parameters[$method][$parameter], ENT_QUOTES, 'UTF-8');
+        } else {
+            return !is_null($default) ? $default : null;
+        }
+
+        return null;
+    }
+        
+    /**
+     * getInt
+     *
+     * @param  string $method
+     * @param  string $parameter
+     * @return mixed
+     */
+    public function getInt($method, $parameter)
+    {
+        return filter_var($this->getParameter($method, $parameter), FILTER_VALIDATE_INT) ? $this->getParameter($method, $parameter) : null;
+    }
+        
+    /**
+     * getBoolean
+     *
+     * @param  string $method
+     * @param  string $parameter
+     * @return mixed
+     */
+    public function getBoolean( $method, $parameter)
+    {
+        return filter_var($this->parameters[$method][$parameter], FILTER_VALIDATE_BOOLEAN);
+    }
+    
+    /**
+     * getEmail
+     *
+     * @param  string $method
+     * @param  string $parameter
+     * @return mixed
+     */
+    public function getEmail( $method, $parameter)
+    {
+        return filter_var($this->getParameter($method, $parameter), FILTER_VALIDATE_EMAIL) ? $this->getParameter($method, $parameter) : null;
+    }
+}
